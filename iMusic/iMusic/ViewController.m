fdf9298f2f7d29099fd7a73c104a4a5aa7f30ced
XiaoChenYung <18730222889@126.com>
2015-10-30 23:35:21 +0800

    自动布局框架导入

--- iMusic/iMusic/ViewController.m
+++ iMusic/iMusic/ViewController.m
@@ -7,21 +7,62 @@
 //
 
 #import "ViewController.h"
-
+#import <Masonry.h>
 @interface ViewController ()
-
+@property (nonatomic ,strong) UILabel *lable2;
+@property (nonatomic ,strong) UILabel *lable1;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.view addSubview:self.lable1];
+    [self.view addSubview:self.lable2];
+    
+
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillLayoutSubviews {
+    [self.lable1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.lessThanOrEqualTo(self.view.mas_top).with.offset(50);
+        make.left.lessThanOrEqualTo(self.view.mas_left).with.offset(150);
+    }];
+    
+    [self.lable2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.lable1).with.offset(0);
+        make.right.equalTo(self.lable1).with.offset(100);
+    }];
+    
+//    [self.lable1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//
+//    }];
+    //[self.view layoutIfNeeded];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark --- lazy
+
+- (UILabel *)lable1 {
+    if (_lable1 == nil) {
+        _lable1 = [[UILabel alloc] init];
+        _lable1.text = @"啦啦啦啦";
+    }
+    return _lable1;
+}
+
+- (UILabel *)lable2 {
+    if (_lable2 == nil) {
+        _lable2 = [[UILabel alloc] init];
+        _lable2.text = @"哈哈哈哈";
+    }
+    return _lable2;
 }
 
 @end
