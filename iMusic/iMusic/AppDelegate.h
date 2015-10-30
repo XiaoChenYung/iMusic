2fb34143f862b610858188b69fd97f59bf14d67d
XiaoChenYung <18730222889@126.com>
2015-10-30 21:56:02 +0800

    初始化

---
+++ iMusic/iMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMusic
+//
+//  Created by 杨晓晨 on 15/10/30.
+//  Copyright © 2015年 杨晓晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
